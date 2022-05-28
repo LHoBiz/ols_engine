cf324a82188a7bd82bdf935e53e16d921dcc4a40
lhodgson <lhodgson@esriaustralia.com.au>
2022-05-29 01:55:00 +1000

    bugfixing init scripts

--- install_conda_ec2.sh
+++ install_conda_ec2.sh
@@ -2,9 +2,8 @@
 
 /miniconda3/bin/conda init
 source ~/.bashrc
-conda create -n "ols_engine" python=3.8 gdal -y
-source ~/.bashrc
+conda create -n ols_engine python=3.8 -y
 conda activate ols_engine
-source ~/.bashrc
-conda install gdal=3.3.x django -y && conda install -c conda-forge postgresql -y && conda install -c conda-forge/label/gcc7 postgresql -y && conda install -c conda-forge/label/broken postgresql -y && conda install -c conda-forge/label/cf201901 postgresql -y && conda install -c conda-forge/label/cf202003 postgresql -y
+conda install gdal poppler tiledb=2.2 -y 
+conda install -c conda-forge postgresql -y && conda install -c conda-forge/label/gcc7 postgresql -y && conda install -c conda-forge/label/broken postgresql -y && conda install -c conda-forge/label/cf201901 postgresql -y && conda install -c conda-forge/label/cf202003 postgresql -y
 
