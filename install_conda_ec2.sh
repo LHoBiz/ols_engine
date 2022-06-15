23446f438304233f52c03d62639344a1132d601d
LHoBiz <magiva_aussie@hotmail.com>
2022-06-16 02:01:57 +1000

    Update install_conda_ec2.sh
    added extra source ~/.bashrc after activating ols_engine env

--- install_conda_ec2.sh
+++ install_conda_ec2.sh
@@ -4,6 +4,6 @@
 source ~/.bashrc
 conda create -n ols_engine python=3.8 -y
 conda activate ols_engine
+source ~/.bashrc
 conda install gdal poppler tiledb=2.2 -y 
 conda install -c conda-forge postgresql -y && conda install -c conda-forge/label/gcc7 postgresql -y && conda install -c conda-forge/label/broken postgresql -y && conda install -c conda-forge/label/cf201901 postgresql -y && conda install -c conda-forge/label/cf202003 postgresql -y
-
