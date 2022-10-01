a7d23d1adbcfca2bdab3def761f6757c6400aebc
lhodgson <lhodgson@esriaustralia.com.au>
2022-10-01 23:49:42 +1000

    updated .sh files so conda installs using ec2-user and repo as well

--- install_conda_ec2.sh
+++ install_conda_ec2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cd /home/ec2-user
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh -b
+
+# conda env export > environment.yml
+git clone https://github.com/LHoBiz/ols_engine.git
+git fetch
+git checkout aws-linux
+# sudo chown -R ec2-user /home/ec2-user/*
+# sudo rm /home/ec2-user/ols_engine/v0.2/ -rf
+
 /miniconda3/bin/conda init
 source ~/.bashrc
 conda create -n ols_engine python=3.8 -y
