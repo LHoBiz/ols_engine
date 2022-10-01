49b3055cb6a67d50121906d87e168ad0b5da305e
lhodgson <lhodgson@esriaustralia.com.au>
2022-10-02 03:09:32 +1000

    hotfix

--- install_geoserver.sh
+++ install_geoserver.sh
@@ -3,6 +3,18 @@
 # from https://docs.geoserver.org/latest/en/user/installation/linux.html
 sudo amazon-linux-extras install java-openjdk11
 cd ~
+
+sudo rm -rf /usr/share/tomcat
+sudo mkdir /usr/share/tomcat
+cd /usr/share/tomcat
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.26/bin/apache-tomcat-10.0.26.tar.gz
+tar xvzf apache-tomcat-10.0.26.tar.gz
+
+cd apache-tomcat-10.0.26
+./configure
+make
+sudo make install
+
 sudo rm -rf /usr/share/geoserver
 sudo mkdir /usr/share/geoserver
 cd /usr/share/geoserver
