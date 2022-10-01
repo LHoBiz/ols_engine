6967d113b89cc0e22124f2a9032b3c720dbc922b
lhodgson <lhodgson@esriaustralia.com.au>
2022-10-02 00:49:43 +1000

    hotfix

--- install_conda_ec2.sh
+++ install_conda_ec2.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b
 
-/miniconda3/bin/conda init
+~/miniconda3/bin/conda init
 source ~/.bashrc
 conda config --append envs_dirs /home/ec2-user/envs
 conda remove --name ols_engine --all -y
