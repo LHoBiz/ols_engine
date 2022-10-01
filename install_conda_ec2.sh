7f1e08ee840b64d32e2bd73a4e41f5f658591106
lhodgson <lhodgson@esriaustralia.com.au>
2022-10-02 00:08:43 +1000

    hotfix

--- install_conda_ec2.sh
+++ install_conda_ec2.sh
@@ -5,5 +5,5 @@ source ~/.bashrc
 conda create -n ols_engine python=3.8 -y
 conda activate ols_engine
 source ~/.bashrc
-sudo conda install gdal poppler tiledb=2.2 -y 
+conda install gdal poppler tiledb=2.2 -y 
 sudo conda install -c conda-forge postgresql -y && conda install -c conda-forge/label/gcc7 postgresql -y && conda install -c conda-forge/label/broken postgresql -y && conda install -c conda-forge/label/cf201901 postgresql -y && conda install -c conda-forge/label/cf202003 postgresql -y
