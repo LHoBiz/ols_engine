164cb0577c40f03bf8f1021e89b831e99512cc72
lhodgson <lhodgson@esriaustralia.com.au>
2022-10-02 02:41:26 +1000

    hotfix

--- install_geoserver.sh
+++ install_geoserver.sh
@@ -2,10 +2,13 @@
 
 # from https://docs.geoserver.org/latest/en/user/installation/linux.html
 sudo amazon-linux-extras install java-openjdk11
+cd ~
+sudo rm -rf /usr/share/geoserver
 sudo mkdir /usr/share/geoserver
-sudo wget https://ixpeering.dl.sourceforge.net/project/geoserver/GeoServer/2.21.1/geoserver-2.21.1-bin.zip -P /usr/share/geoserver
+cd /usr/share/geoserver
+sudo wget https://ixpeering.dl.sourceforge.net/project/geoserver/GeoServer/2.21.1/geoserver-2.21.1-bin.zip 
 sudo unzip geoserver-2.21.1-bin.zip
 echo "export GEOSERVER_HOME=/usr/share/geoserver" >> ~/.profile
 . ~/.profile
 sudo chown -R ec2-user /usr/share/geoserver/
-sh /usr/share/geoserver/bin/startup.sh
+sh ./bin/startup.sh
